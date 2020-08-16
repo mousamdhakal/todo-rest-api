f4e8501ba14f1e806bde18cb468842767e4054ab
mousamdhakal <mousamdhakal7.md@gmail.com>
2020-08-16 17:44:13 +0545

    Add cors handling further

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(
 app.use(express.json());
 
 // Handle cors error
-// app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
@@ -29,6 +28,7 @@ app.use((req, res, next) => {
   }
   next();
 });
+app.use(cors());
 
 // Set up api for use
 app.use('/api', apiRoute);
